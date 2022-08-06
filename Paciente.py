a5e53eb885511871a8f71c8950cdcdfe527308c1
Nico <nicolascalata93@gmail.com>
2022-08-05 21:35:57 -0300

    archivo Paciente.py modificado

--- Paciente.py
+++ Paciente.py
@@ -27,13 +27,3 @@ class Paciente(Persona):
     def GetObraSocial(self):
         return self.obrasocial
         
-        
-        
-        
-# El paciente arranca con un turno vacío
-# no es lo mismo que null, pero para que lo entiendas
-#arranca con un turno vacio, el agregar lo que hace es chequear si está vacio,
-# y en ese caso agrega un turno con los parametros que recibe. 
-# El eliminar turno lo deja vacio otra vez. 
-# Y el imprimir turno lo que hace es un print del turno.getTurno
-        
